6111d1bfb66a65b5a03a6181719c6de14a12cde6
Dhruv Rathi <rathidhruv04@gmail.com>
2022-02-11 00:14:57 +0530

    Made a Basic Search Page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
+import Search from "./Component/Search";
+
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Router>
+    <div className="container my-3">
+    <Switch>
+          <Route exact path="/">
+            <Search/>
+          </Route>
+          <Route exact path="/about">
+           
+          </Route>
+    </Switch>
     </div>
+    </Router>
+    </> 
   );
 }
 
